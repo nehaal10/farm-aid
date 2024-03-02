482237207232353c710cd801b6f38ed95c520686
kd14 <kd14@ford.com>
2024-03-02 22:45:33 +0530

    minor bugs_llm

--- backend/custom_llm.py
+++ backend/custom_llm.py
@@ -1,29 +1,55 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders.unstructured import UnstructuredFileLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
-from langchain.llms.openai import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from dotenv import load_dotenv
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+from langchain_community.llms.openai import OpenAI
 import os
 
-# insert open ai key
+OPENAI_API_KEY="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"
+PINECONE_API_KEY="f342e959-7272-4a04-a0ef-6b8257f392c0"
+os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
 
+# load the pdf
 file = "data/farmerbook.pdf"
-loader = UnstructuredFileLoader(file)
+loader = UnstructuredPDFLoader(file)
 documents = loader.load()
+# i am prints the no document and characters in document
+print(f'You have {len(documents)} document(s) in your data')
+print(f'There are {len(documents[0].page_content)} characters in your document')
 
-spliter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+# The pdf is splitted in small chucks of documents
+spliter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
 data = spliter.split_documents(documents)
 
-# embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+print(f'Now you have {len(data)} documents')
 
-vectorestore = Chroma.from_documents(data,embeddings)
+
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+
+# Create pf pinecone once alone run it .
+# index_name="langchain1"
+# pc.create_index(
+#     name='langchain3', 
+#     dimension=1536, 
+#     metric="cosine", 
+#     spec=PodSpec(environment="gcp-starter")
+# )
+index_name="langchain3"
+docsearch = PineconeVectorStore.from_documents(data, embeddings, index_name=index_name)
+
+query = "What did the president say about Ketanji Brown Jackson"
+docs = docsearch.similarity_search(query)
+print(docs)
+
+
+
+# vectorestore = Chroma.from_documents(data,embeddings)
 
 # llm = OpenAI(temperature = 0 , open_api_key=OPENAI_API_KEY)
-chain = load_qa_chain(llm, chain_type="stuff")
+# chain = load_qa_chain(llm, chain_type="stuff")
 
-query = "whats the ideal distance between mango trees"
-docs = vectorestore.similarity_search(query)
+# query = "whats the ideal distance between mango trees"
+# docs = vectorestore.similarity_search(query)
 
-chain.run(input_documents=docs, question=query)
+# chain.run(input_documents=docs, question=query)
